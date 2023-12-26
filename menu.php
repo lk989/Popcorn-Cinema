9f645e50e8ce1803d762770fa992a77a71ace4e2
shahdAls <128863844+shahdAls@users.noreply.github.com>
2023-12-26 23:17:10 +0300

    hello

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-	<title>Menu</title>
+	<title>Menu sh</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400&display=swap" rel="stylesheet">
