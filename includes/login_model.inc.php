1a2f86f7420239b6f6003c98adde770d0af93925
shahdAls <128863844+shahdAls@users.noreply.github.com>
2024-02-10 16:24:57 +0300

    u

--- includes/login_model.inc.php
+++ includes/login_model.inc.php
@@ -1,13 +1,12 @@
 <?php
-
+declare(strict_types=1);
 function get_user( object $pdo,  string $username){
 
 
-        $query="SELECT * FROM user WHERE username= :username;";
+        $query = "SELECT * FROM user WHERE username = :username";
         $stmt = $pdo->prepare($query);
-        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":username", $username, PDO::PARAM_STR);
         $stmt->execute();
-        
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         return $result;
         
