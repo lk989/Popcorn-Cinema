af39460f9727951fcec2a83b21c31660a6b1d018
lk989 <lma.bugis@hotmail.com>
2024-02-11 22:39:04 +0300

    make showing now and coming soon dynamic

--- coming-soon.php
+++ coming-soon.php
@@ -14,54 +14,44 @@
 
 <body>
 
-    <?php 
+    <?php
+        include 'queries.php';
+        $all_coming_soon = fetch_all('movie', true, 'showing_now = "0"', null);
         include('header.php');
     ?>
 
     <br><br>
     <h1 id="Soon"> Comming Soon </h1>
 
-    <div>
-        <table id="Movies" align="center">
-
-            <tr>
-                <td><a href="spider-man.html"><img
-                            src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQFHMCMi3SM6HVf0M-Ua0zLlXgBvdsyTT8JL47mVS58uxEaEPse"
-                            width="200" height="300"><br>Spider-Man<br>Action/Adventure</a></td>
-                <td><a href="wish.html"><img
-                            src="https://www.muvicinemas.com/_next/image?url=https%3A%2F%2Fd3th1nqbpcyfdw.cloudfront.net%2F1683544836865-774db6d1-83bf-432f-a924-06493e4e54f3.jpg&w=3840&q=75"
-                            width="200" height="300"><br>Wish<br>Comedy/Adventure</a></td>
-                <td><img src="https://images.justwatch.com/poster/304329114/s718/mission-impossible-7.%7Bformat%7D"
-                        width="200" height="300"><br>Mission: Impossible<br>Action/Adventure</td>
-                <td><img src="https://upload.wikimedia.org/wikipedia/ar/thumb/1/13/The_Flash_film_poster_araby.png/260px-The_Flash_film_poster_araby.png"
-                        width="200" height="300"><br>The Flash<br>Action/Adventure</td>
-            </tr>
-
-            <tr>
-                <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIvVvvDSlYhIlLTRdf0Ww1iAoTQjXEylD61-mQ9ghoPYvZAhhx"
-                        width="200" height="300"><br>Extraction 2<br>Action/Thriller</td>
-                <td><img src="https://images.justwatch.com/poster/301818741/s592/suzume-no-tojimari" width="200"
-                        height="300"><br>Suzume<br>Adventure/Animation</td>
-                <td><img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSUmeqrk-sWsis5BcoOhv8PQ_HgoKfCodR2gCDfreWqzkipZ3uF"
-                        width="200" height="300"><br>The Boogeyman<br>Horror/Mystery</td>
-                <td><img src="https://lumiere-a.akamaihd.net/v1/images/p_disney_elemental_v3_793_2a328b27.jpeg?region=0%2C0%2C540%2C810"
-                        width="200" height="300"><br>Elemental<br>Animation/Comedy</td>
-
-            </tr>
-
-            <tr>
-                <td><img src="https://pbs.twimg.com/media/FvUVt3hXgAAxP1H?format=jpg&name=900x900" width="200"
-                        height="300" id="img9"><br>Oppenheimer<br>Drama/War</td>
-                <td><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSD1MIbeRg7PJAaYo-kx7woXDCUHmYOBW2nAqlQdBEwLuqnJIUH"
-                        width="200" height="300"><br>The Roundup No Way Out<br>Adventure</td>
-                <td><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSMnMLOYARaIAuDNAejIcbXu3bWNcUyWKj87Pvh1XLooioIiRro"
-                        width="200" height="300"><br>Meg 2: The Trench<br>Adventure</td>
-                <td><img src="https://m.media-amazon.com/images/M/MV5BYTNmYWIwOWUtNzk0ZC00MjVmLTkyOWYtZGZhYTMwNTAxYTE5XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg"
-                        width="200" height="300"><br>
-                    Lonely Castle in the Mirror<br>Animation</td>
-
-            </tr>
-        </table>
+    <div class="grid-container">
+    <?php 
+        while($coming_soon=$all_coming_soon->fetch_assoc())
+            {
+        ?>
+                <div class="grid-item">
+                    <a href="movie_details.php?id=<?php echo $coming_soon['id'];?>">
+                        <article class="img-posters">
+                            <img
+                                src="<?php echo $coming_soon['poster'];?>">
+                            <article class="poster-content">
+                                <h3><?php echo $coming_soon['name'];?></h3><br>
+                                <p><?php echo $coming_soon['language'];?></p><br>
+                                <p>
+                                <?php 
+                                    $genres = fetch_all('genre', false, 'id in (SELECT genre_id FROM movie_genres WHERE movie_id = ' . $coming_soon['id'] . ')', null);
+                                    while($genre=$genres->fetch_assoc())
+                                    {
+                                        echo $genre['name'] . ' . ';
+                                    }
+                                ?>
+                                </p>
+                            </article>
+                        </article>
+                    </a>
+                </div>
+        <?php
+            }
+        ?>
     </div>
 
     <footer class="movie-footer">
--- showing-now.php
+++ showing-now.php
@@ -14,56 +14,44 @@
 </head>
 
 <body>
-    <?php 
+    <?php
+        include 'queries.php';
+        $all_showing_now = fetch_all('movie', true, 'showing_now = "1"', null);
         include('header.php');
     ?>
     <br><br>
     <h1 class="showing"> Showing Now : </h1>
 
-    <table id="Movies">
-        <tr>
-            <td><a href="mario.html"><img
-                        src="https://etbilarabi.com/sites/default/files/inline-/images/0BE55DEF-CCC7-49EE-A52F-51BC31125588.jpeg"
-                        width="200" height="300"><br>The Super Mario Bros<br>Adventure/Fantasy</a></td>
-            <td><a href="mermaid.html"><img
-                        src="https://www.muvicinemas.com/_next/image?url=https%3A%2F%2Fd3th1nqbpcyfdw.cloudfront.net%2F1682905252634-1183e400-822b-468f-8003-dee7995ffbda.png&w=3840&q=75"
-                        width="200" height="300"><br>The Little Mermaid<br>Adventure/Fantasy</a></td>
-            <td><img src="https://www.themoviedb.org/t/p/original/8Ga1CI4ZIIF8fxyfjZ5sNlb75e4.jpg" width="200"
-                    height="300"><br> A Whisker Away<br>Fantasy/Romance</td>
-            <td><img src="https://th.bing.com/th/id/OIP.m_OYFvtzjIOnppssXLg3AgHaK9?pid=ImgDet&rs=1" width="200"
-                    height="300"><br>Adam Project<br>Sci-fi/Drama</td>
-        </tr>
-
-
-        <tr>
-
-            <td><img src="https://th.bing.com/th/id/OIP.Dgv015cofac8Mv-HwImQYwAAAA?pid=ImgDet&rs=1" width="200"
-                    height="300"><br>Turning Red<br>Fantasy/Comedy</td>
-            <td><img src="https://th.bing.com/th/id/R.53181b5658f102b10ca1f22c64bcdbce?rik=m2EPXF%2bwKaTwFA&pid=ImgRaw&r=0"
-                    width="200" height="300"><br>Top Gun: Maverick<br>Action/Adventure</td>
-            <td><img src="https://cdn.traileraddict.com/content/pixar-disney/coco-2017-4.jpg" width="200"
-                    height="300"><br>Coco<br>Family/Comedy</td>
-            <td><img src="https://www.bing.com/th?id=OIP.At011FrNqyDEtglatOSGGwHaK9&w=150&h=222&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
-                    width="200" height="300"><br>Seoul Vibe<br>Crime</td>
-
-        </tr>
-
-
-        <tr>
-
-            <td><img src="https://image.tmdb.org/t/p/original/esbfojkHBEyMlIcv2VGW3fCzlzH.jpg" width="200"
-                    height="300"><br>Zootopia<br>Family/Adventure</td>
-            <td><img src="https://th.bing.com/th/id/R.011d0f9ceea10e0f9ab2fc8d1fff6ddd?rik=46tRZhLjPLn6SA&pid=ImgRaw&r=0"
-                    width="200" height="300"><br>Loca<br>Comedy/Fantasy</td>
-            <td><img src="https://www.muvicinemas.com/_next/image?url=https%3A%2F%2Fd3th1nqbpcyfdw.cloudfront.net%2F1683022287134-909a752f-b7d8-4e53-8ae6-55b10e29f204.jpg&w=3840&q=75"
-                    width="200" height="300"><br>Gohn Wick: Chapter 4<br>Action/Neo-noir</td>
-            <td><img src="https://www.muvicinemas.com/_next/image?url=https%3A%2F%2Fd3th1nqbpcyfdw.cloudfront.net%2F1684766169897-59a5021e-e25c-4e97-b930-72bdefaa10fc.jpg&w=3840&q=75"
-                    width="200" height="300"><br>The Boogeyman<br>Horror/Mystery</td>
-
-        </tr>
-
-
-    </table>
+    <div class="grid-container">
+    <?php 
+        while($showing_now=$all_showing_now->fetch_assoc())
+            {
+        ?>
+                <div class="grid-item">
+                    <a href="movie_details.php?id=<?php echo $showing_now['id'];?>">
+                        <article class="img-posters">
+                            <img
+                                src="<?php echo $showing_now['poster'];?>">
+                            <article class="poster-content">
+                                <h3><?php echo $showing_now['name'];?></h3><br>
+                                <p><?php echo $showing_now['language'];?></p><br>
+                                <p>
+                                <?php 
+                                    $genres = fetch_all('genre', false, 'id in (SELECT genre_id FROM movie_genres WHERE movie_id = ' . $showing_now['id'] . ')', null);
+                                    while($genre=$genres->fetch_assoc())
+                                    {
+                                        echo $genre['name'] . ' . ';
+                                    }
+                                ?>
+                                </p>
+                            </article>
+                        </article>
+                    </a>
+                </div>
+        <?php
+            }
+        ?>
+    </div>
 
     <footer class="movie-footer">
         <h1>Old Movies suggestion to show it in the cinema :</h1>
