59cd937509614acd705242562a1790e469dbf819
shahdAls <128863844+shahdAls@users.noreply.github.com>
2024-02-12 15:51:57 +0300

    update header

--- ShowBill.php
+++ ShowBill.php
@@ -31,25 +31,11 @@
           
         }
     ?>
-
-    <header>
-        <nav>
-            <ul>
-                <li><a href="coming-soon.php" class="wide nav-links">Coming soon</a></li>
-                <li><a href="offers.php" class="nav-links">Offers</a></li>
-                <li><a href="index.php"><img src="images/logo.png" id="logo"></a></li>
-                <li><a href="showing-now.php" class="nav-links">Movies</a></li>
-                <li><a href="menu.php" class="nav-links">Menu</a></li>
-            </ul>
-        </nav>
-        <div id="sign-in">
-            <div class="arc"></div>
-            <a href="login.php">
-                <p>Login / Sign Up</p>
-            </a>
-        </div>
-    </header>
-    <br><br>
+    
+ <?php
+      include('header.php');
+    ?>
+   
 <body>
 
 <style>
